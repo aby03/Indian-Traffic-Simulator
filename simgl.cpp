7f6346f4ed39cded2f0949809a9e3177c3452b05
Kaustubh <kaustubh698@gmail.com>
2019-03-11 23:24:04 +0530

    Spherical coordinates

--- simgl.cpp
+++ simgl.cpp
@@ -14,16 +14,16 @@ float uz = 0.1f;
 // float gy = 0.15f;
 
 // angle of rotation for the camera direction
-float angleX=0.0;
-float angleY=0.0;
+float angle1=0.0;
+float angle2=0.0;
 
 
 // Camera coordinates
 float x=0.0f, y=0.0f, z=5.0f;
 // actual vector representing the camera's direction
 float lx=0.0f, ly=0.0f, lz=-1.0f;
-// XZ position of the camera
-float r=5.0f, rz=5.0f;
+// Spherical position of the camera
+float r=5.0f;
 
 int roadLength = 21;
 int roadWidth = 6;
@@ -35,6 +35,10 @@ void drawCar(int carl, int carw)
 	float carX = carl*ux/2.0;
 	float carY = carw*uy/2.0;
 	float carZ = 0.1f;
+
+	//To create gaps in between adjacent cars
+	carX -= 0.03f;
+	carY -= 0.03f; 
 
 	glPushMatrix();
 
@@ -94,12 +98,44 @@ void drawCar(int carl, int carw)
     glVertex3f(-carX, -carY, carZ);
     glEnd();
 
+
+    //Square Pyramid Top
+    glBegin(GL_TRIANGLES);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0, 0, 2*carZ);
+    glVertex3f(carX, carY, carZ);
+    glVertex3f(-carX, carY, carZ);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0, 0, 2*carZ);
+    glVertex3f(-carX, -carY, carZ);
+    glVertex3f(-carX, carY, carZ);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0, 0, 2*carZ);
+    glVertex3f(carX, -carY, carZ);
+    glVertex3f(-carX, -carY, carZ);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0, 0, 2*carZ);
+    glVertex3f(carX, carY, carZ);
+    glVertex3f(carX, -carY, carZ);
+    glEnd();
+    //
+
+    //Wheel
+    
+
     glPopMatrix;
 }
 
-void renderScene()
-{
-
+void renderScene(){
     // if (deltaMove)
     //     computePos(deltaMove);
     // if (deltaAngle)
@@ -143,14 +179,15 @@ void renderScene()
     glVertex3f(rl, -rw, 0);
     glEnd();
 
-    float initX = -((roadLength+1.0)/2)*ux;
+    float initX = -((roadLength-1.0)/2)*ux;
     float initY = ((roadWidth-1.0)/2)*uy;
 
-    //Translate to position (0,0) on road
-	glTranslatef(initX, initY, 0.1f);    
+    //Translate to position (0,0) on road 
+    //It is the centre point of 0.1x0.1 box
+	glTranslatef(initX, initY, 0.0f);
 
     glPushMatrix();
-    glTranslatef(3*ux, 0, 0); //Translate to that car's coordinates!
+    glTranslatef(2*ux, 0, 0); //Translate to that car's coordinates!
     drawCar(3,2);
     glPopMatrix();
 
@@ -204,38 +241,37 @@ void processSpecialKeys(int key, int xx, int yy) {
 
     switch (key) {
 		case GLUT_KEY_LEFT :
-			angleX -= 0.05f;
-			x = r*sin(angleX);
-			z = rz*cos(angleX);
+			angle1 -= 0.05f;
 			break;
 		case GLUT_KEY_RIGHT :
-			angleX += 0.05f;
-			x = r*sin(angleX);
-			z = rz*cos(angleX);
+			angle1 += 0.05f;
 			break;
         case GLUT_KEY_UP :
-			angleY += 0.05f;
-			y = r*sin(angleY);
-			z = rz*cos(angleY);
+			angle2 += 0.05f;
 			break;
         case GLUT_KEY_DOWN :
-			angleY -= 0.05f;
-			y = r*sin(angleY);
-			z = rz*cos(angleY);
+			angle2 -= 0.05f;
 			break;
+		case GLUT_KEY_PAGE_UP:
+			r -= 0.1f;
+			break;
+		case GLUT_KEY_PAGE_DOWN:
+			r += 0.1f;
+			break;
+
     }
+
+    x = r*cos(angle2)*sin(angle1);
+	z = r*cos(angle2)*cos(angle1);
+	y = r*sin(angle2);
+
 }
 
-// void update(int value){
-//     // carx += 0.01f;
-//     rX += 1.0f;
-//     rY += 1.0f;
-//     // if (rX > 360){
-//     // 	rX = 0;
-//     // }
-//     glutPostRedisplay(); // Inform GLUT that the display has changed
-//     glutTimerFunc(25,update,0);//Call update after each 25 millisecond
-// }
+void update(int value){
+	//doSomething
+    glutPostRedisplay(); // Inform GLUT that the display has changed
+    glutTimerFunc(1000,update,0);//Call update after each 25 millisecond
+}
 
 
 int main(int argc, char **argv)
@@ -256,8 +292,9 @@ int main(int argc, char **argv)
 
     // glutIgnoreKeyRepeat(1);
     // glutSpecialUpFunc(pressKey);        
+
     // Add a timer for the update(...) function
-    // glutTimerFunc(25, update, 0);
+    // glutTimerFunc(1000, update, 0);
 
     glutMainLoop();
     return 0;
